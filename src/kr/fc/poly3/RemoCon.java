18bf78d6d2834fcdaca97322ea030b9c84b8e54f
may <parkjh080@naver.com>
2023-08-04 13:31:51 +0900

    feat(study): Learn about Interface of JAVA programming 23-08-04
    Today, learn about Interface and difference between Interface and
    Abstract class

---
+++ src/kr/fc/poly3/RemoCon.java
@@ -0,0 +1,14 @@
+package kr.fc.poly3;
+
+public interface RemoCon {
+    //공통된 기능만 넣어야 한다.
+    public void chUP();
+
+    public void chDown();
+
+    public void volUp();
+
+    public void volDown();
+
+    //  public void internet(); 공통된 기능이 아니라면 무의미 해진다.
+}
---
+++ src/kr/fc/poly3/TV.java
@@ -0,0 +1,23 @@
+package kr.fc.poly3;
+
+public class TV implements RemoCon{
+    // 부모가 인터페이스면 모든 메소드들을 재정의 해줘야 한다.
+
+    @Override
+    public void chUP() {
+
+    }
+    @Override
+    public void chDown() {
+
+    }
+    @Override
+    public void volUp() {
+
+    }
+    @Override
+    public void volDown() {
+
+    }
+
+}
