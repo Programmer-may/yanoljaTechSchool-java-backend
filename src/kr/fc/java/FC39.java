184f206981f969d5f5aa23a99bcd0d6279395b48
JuHyeonPark <parkjh080@naver.com>
2023-10-04 13:05:32 +0900

    feat(study): Learn about IoC 23-10-04
    Today, Learn about Inversion of Control. MemberService is object
    that
    have a behavior "welcome". To use "MemberService" object, we have
    to
    construct "MemberService" object by ourselves.(FC39.java)
    This is
    what developers do to control the life cycle of an object.

---
+++ src/kr/fc/java/FC39.java
@@ -0,0 +1,10 @@
+package kr.fc.java;
+
+public class FC39 {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberService();
+        memberService.welcome("Kim");
+    }
+    // 객체의 흐름의 제어권이 개발자에게 있다.
+
+}
