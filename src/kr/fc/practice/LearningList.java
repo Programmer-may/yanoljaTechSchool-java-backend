0bd6a42a610077cc547eef5c508dadcc6a385415
JuHyeonPark <parkjh080@naver.com>
2023-11-06 20:25:46 +0900

    feat(study): Learn about java data structure List 23-10-04
    List에는 삭제 메소드인 remove에는 두가지 경우가 있다. 두번째 경우는 remove argument 안에 객체를 넣어서
    해당 객체를 삭제할 수 있다. 객체를 넣어야하기 때문에 원시 타입은 넣으면 인덱스로 인식하기 때문에 Integer 클래스로
    감싸줘야한다. 이때 박싱이 일어난다. 해당 객체를 삭제하면 인덱스 번호에 맞는 값이 아닌 넣어준 값이 삭제된 걸 확인할 수 있다.

--- src/kr/fc/practice/LearningList.java
+++ src/kr/fc/practice/LearningList.java
@@ -13,6 +13,14 @@ public class LearningList {
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
-
+        System.out.println("==========");
+        List<Integer> list2 = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list2.add(i);
+        }
+        list2.remove((Integer)3);
+        for (int i = 0; i < list2.size(); i++) {
+            System.out.println(list2.get(i));
+        }
     }
 }
